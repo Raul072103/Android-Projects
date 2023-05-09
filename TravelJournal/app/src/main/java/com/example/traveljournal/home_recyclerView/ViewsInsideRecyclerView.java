81d2d15d9651f2ca2d3736658f231365806571c1
raul <raulandrei2017@yahoo.com>
2023-05-09 16:52:24 +0300

    Travel journal, splash screen,navigation drawer added, working on adding trips

---
+++ TravelJournal/app/src/main/java/com/example/traveljournal/home_recyclerView/ViewsInsideRecyclerView.java
@@ -0,0 +1,16 @@
+package com.example.traveljournal.home_recyclerView;
+
+import android.widget.ImageView;
+import android.widget.RatingBar;
+
+public class ViewsInsideRecyclerView {
+
+    private String tripName;
+    private String destination;
+    private ImageView imageView;
+    private RatingBar ratingBar;
+
+    
+
+
+}
